f4546edf555c5ff448aa7b2cedb72215c90dd6d3
fgarcia-student <fgarcia.student04@gmail.com>
2017-06-24 00:21:50 -0400

    cleanup redundant code

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ let path = require('path');
 let app = express();
 
 app.use(express.static('./public/course_files_export'));
-app.use('/', serveIndex('./public/course_files_export',{'setHeaders' : setHeaders}));
-
-function setHeaders (res, path) {
-  res.setHeader('Content-Disposition', contentDisposition(path))
-}
+app.use('/', serveIndex('./public/course_files_export'));
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("listening on ", (process.env.PORT || 3000));
