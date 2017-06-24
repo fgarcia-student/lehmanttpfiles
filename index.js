a86b390cb0599b713d372a134f58ace445cead3a
fgarcia-student <fgarcia.student04@gmail.com>
2017-06-23 23:53:35 -0400

    serving static dir

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let express = require('express');
+let serveIndex = require('serve-index');
+let path = require('path');
 
 let app = express();
 
-app.set(express.static(''))
+app.use('/', serveIndex('./public/course_files_export'));
+
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log("listening on ", (process.env.PORT || 3000));
+});
