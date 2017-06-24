99db17df0dfa75404af06e8fdc235a79477d73ab
fgarcia-student <fgarcia.student04@gmail.com>
2017-06-24 00:07:23 -0400

    serving files on click

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ let path = require('path');
 
 let app = express();
 
-app.use('/', serveIndex('./public/course_files_export'));
+app.use(express.static('./public/course_files_export'));
+app.use('/', serveIndex('./public/course_files_export',{'setHeaders' : setHeaders}));
 
+function setHeaders (res, path) {
+  res.setHeader('Content-Disposition', contentDisposition(path))
+}
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("listening on ", (process.env.PORT || 3000));
